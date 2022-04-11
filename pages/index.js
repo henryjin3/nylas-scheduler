f81f5d1e3c7a6322fe392d646fe3d5e1b0642e52
henryjin3 <henry@cadenzadevelopment.com>
2022-04-11 13:55:39 +0800

    Add availability component with dynamic import

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,17 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { useEffect } from 'react';
+import dynamic from 'next/dynamic';
+
+const NylasAvailability = dynamic(
+  () => import('@nylas/components-availability'),
+  { ssr: false }
+);
 
 export default function Home() {
-  useEffect(() => import('@nylas/components-availability'), []);
   return (
     <div className={styles.container}>
-      <nylas-availability id="0a05ab2d-266c-4551-8b52-459104f2f5bf"></nylas-availability>
+      <NylasAvailability id="0a05ab2d-266c-4551-8b52-459104f2f5bf"></NylasAvailability>
     </div>
   );
 }
