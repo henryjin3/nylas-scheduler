02091182c05f169da3947f36fcb2c846688d5d18
henryjin3 <henry@cadenzadevelopment.com>
2022-04-11 13:58:00 +0800

    Switch to CDN import

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,11 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import { useEffect } from 'react';
-import dynamic from 'next/dynamic';
-
-const NylasAvailability = dynamic(
-  () => import('@nylas/components-availability'),
-  { ssr: false }
-);
+import Script from 'next/script';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <NylasAvailability id="0a05ab2d-266c-4551-8b52-459104f2f5bf"></NylasAvailability>
+      <Script src="https://unpkg.com/@nylas/components-availability"></Script>
+      <nylas-availability id="0a05ab2d-266c-4551-8b52-459104f2f5bf"></nylas-availability>
     </div>
   );
 }
